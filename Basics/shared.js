30c9fdc5606992fb3bbbf049e3b16d943ab37529
Kelvan Ince <kelvan@mirafinancial.com>
2018-12-11 21:34:18 -0500

    Add toggle button and sidebar

--- Basics/shared.js
+++ Basics/shared.js
@@ -1,8 +1,9 @@
 var backdrop = document.querySelector('.backdrop');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var modal = document.querySelector('.modal');
-
 var closeButtons = document.querySelectorAll('.modal__actions');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
@@ -11,14 +12,24 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 
-for (let i = 0; i < closeButtons.length; i++) {
-  closeButtons[i].addEventListener('click', () => {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
-  });
+if (closeButtons) {
+  for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', () => {
+      if (modal) {
+        modal.style.display = 'none';
+      }
+      backdrop.style.display = 'none';
+    });
+  }
 }
 
 backdrop.addEventListener('click', () => {
   modal.style.display = 'none';
   backdrop.style.display = 'none';
+  mobileNav.style.display = 'none';
+});
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
 });
