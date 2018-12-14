ee8a3f16ebcd6ed9d908576ff0876293969d1405
Kelvan Ince <kelvan@mirafinancial.com>
2018-12-13 21:14:04 -0500

    Add media query to main css, increasing title on mobile screen

--- Basics/shared.js
+++ Basics/shared.js
@@ -1,35 +1,45 @@
-var backdrop = document.querySelector('.backdrop');
-var selectPlanButtons = document.querySelectorAll('.plan button');
-var modal = document.querySelector('.modal');
-var closeButtons = document.querySelectorAll('.modal__actions');
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
-for (let i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function() {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 
-if (closeButtons) {
-  for (let i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', () => {
-      if (modal) {
-        modal.style.display = 'none';
-      }
-      backdrop.style.display = 'none';
-    });
-  }
-}
-
-backdrop.addEventListener('click', () => {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
-  mobileNav.style.display = 'none';
+backdrop.addEventListener("click", function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
 });
 
-toggleButton.addEventListener('click', () => {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+}
+
+toggleButton.addEventListener("click", function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
